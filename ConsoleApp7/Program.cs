40c4a3b91c70a76c6607b6e293e00b0fc844d2cd
saydi <saydi@ENGINEER>
2022-06-07 18:27:50 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите число X ");
+        int x = int.Parse(Console.ReadLine());
+        Console.WriteLine("Введите число Y ");
+        int y = int.Parse(Console.ReadLine());
+        x = x + y;
+        y = x - y;
+        x = x - y;
+        Console.WriteLine("Ответ:");
+        Console.WriteLine(x);
+        Console.WriteLine(y);
+    }
+}
